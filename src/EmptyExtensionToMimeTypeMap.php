5f4e3b72a4fe6f8f83d9d14ddc46b3403906091e
Frank de Jonge <info@frenky.net>
2020-07-26 10:17:10 +0200

    Remove useless code

--- src/EmptyExtensionToMimeTypeMap.php
+++ src/EmptyExtensionToMimeTypeMap.php
@@ -6,13 +6,8 @@ namespace League\MimeTypeDetection;
 
 class EmptyExtensionToMimeTypeMap implements ExtensionToMimeTypeMap
 {
-    /**
-     * @var string[]
-     */
-    private const MIME_TYPES_FOR_EXTENSIONS = [];
-
     public function lookupMimeType(string $extension): ?string
     {
-        return self::MIME_TYPES_FOR_EXTENSIONS[$extension] ?? null;
+        return null;
     }
 }
