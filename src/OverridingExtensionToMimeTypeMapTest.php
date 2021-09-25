5d40d598dc266d163178ef4c1414a6578a173700
Frank de Jonge <info@frankdejonge.nl>
2021-09-25 10:19:25 +0200

    Added ability to override mime type map values but falling back to the generated one.

---
+++ src/OverridingExtensionToMimeTypeMapTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace League\MimeTypeDetection;
+
+use PHPStan\Testing\TestCase;
+
+class OverridingExtensionToMimeTypeMapTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function overriding_a_mime_type_value(): void
+    {
+        $innerMap = new GeneratedExtensionToMimeTypeMap();
+        $overridingMap = new OverridingExtensionToMimeTypeMap($innerMap, ['mp3' => 'custom/value']);
+
+        $mp3 = $overridingMap->lookupMimeType('mp3');
+        $mp4 = $overridingMap->lookupMimeType('mp4');
+
+        self::assertEquals('custom/value', $mp3);
+        self::assertEquals('video/mp4', $mp4);
+    }
+}
